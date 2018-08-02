47dc651c5b468591f5e94f80bbb4c1c7be64dcb0
ervinismu <social.ervin@gmail.com>
2018-08-02 11:13:03 +0700

    [erpin]ad error handling in rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
+#![feature(plugin)] // use plugin
+#![plugin(rocket_codegen)] //use plugin
 
 extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello World!"
+    "Hello world!"
+}
+
+#[catch(404)]
+fn not_found(req: &rocket::Request) -> String {
+    format!("Sorry, but '{}' is not a valid path!", req.uri())
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .catch(catchers![not_found])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
